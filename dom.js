63532f0266d082f84acc08bbfb48810251fc36da
GitShaAli <shafeeqalibn@outlook.com>
2022-07-01 23:47:55 +0530

    dom update task4

--- dom.js
+++ dom.js
@@ -20,3 +20,14 @@ headerT.style.borderBottom = 'solid 3px #000'
 var additem = document.querySelector('.title');
 additem.style.fontWeight = "bold";
 additem.style.color = 'green';
+
+
+var item = document.getElementsByClassName('list-group-item');
+console.log(item);
+console.log(item[1]);
+item[1].textContent='Hello 2';
+item[2].style.color = 'green';
+
+for(var i =0;i<item.length;i++){
+    item[i].style.fontWeight="bold";
+}
